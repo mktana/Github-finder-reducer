db69cbbed89b6fd527df38b1806b17745435731c
moriya <moriyakatan@gmail.com>
2024-06-05 11:08:44 +0300

    Add new project

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+import { FaHome } from "react-icons/fa"
+import { Link } from "react-router-dom"
+
+function NotFound() {
+  return (
+    <div className="">NotFound</div>
+  )
+}
+
+export default NotFound
